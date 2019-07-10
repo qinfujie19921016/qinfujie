4331fdd29c2fe758844d0e33cc0b9d66ecf48677
qinfujie <804035144@qq.com>
2019-07-10 14:47:08 +0800

    git

---
+++ src/main/java/com/example/qinfujie/QinfujieApplication.java
@@ -0,0 +1,13 @@
+package com.example.qinfujie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QinfujieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QinfujieApplication.class, args);
+    }
+
+}
